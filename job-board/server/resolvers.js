66bf84a5c71e92c56f03be8af0c3df8d4cb8d3d2
Josué Pacheco <josuepacheco@MacBook-Pro-de-Josue.local>
2024-05-13 14:28:38 -0500

    schema job-board

---
+++ job-board/server/resolvers.js
@@ -0,0 +1,18 @@
+import { getCompany } from './db/companies.js'
+import {getJobs} from './db/jobs.js'
+
+export const resolvers ={
+    Query:{
+       jobs : ()=> getJobs()
+    },
+    Job:{
+        date: (job)=> toISODate(job.createdAt),
+        company:(job)=> getCompany(job.companyId)
+
+    }
+}
+
+
+function toISODate(value){
+    return value.slice(0,'yyyy-mm-dd'.length)
+}
